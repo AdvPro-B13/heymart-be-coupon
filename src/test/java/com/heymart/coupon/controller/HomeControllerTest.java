56caf028e5d430dc62e5eae8d861316ca3bbe335
kkuratolodon <mif05123@gmail.com>
2024-04-16 21:41:38 +0700

    [RED] Add Test for HomeController

---
+++ src/test/java/com/heymart/coupon/controller/HomeControllerTest.java
@@ -0,0 +1,34 @@
+package com.heymart.coupon.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@ExtendWith(MockitoExtension.class)
+class HomeControllerTest {
+    MockMvc mockMvc;
+    @InjectMocks
+    HomeController homeController;
+
+    @BeforeEach
+    void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(homeController).build();
+    }
+
+    @Test
+    void testEditProductPage() throws Exception {
+        String supermarket = "Raflimart";
+
+        mockMvc.perform(get("/product/edit/" + supermarket))
+                .andExpect(status().isOk())
+                .andExpect(view().name("Ini isinya list kupon dari " + supermarket + "!"));
+    }
+}
