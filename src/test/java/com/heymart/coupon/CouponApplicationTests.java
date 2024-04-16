2fe4dc7d2efded553041fa572d06db046a1d4014
kkuratolodon <mif05123@gmail.com>
2024-04-16 20:30:03 +0700

    initial commit

---
+++ src/test/java/com/heymart/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heymart.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
