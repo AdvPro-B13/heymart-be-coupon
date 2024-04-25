967d43147ca1a5f00d575cdef7ba94b30d4ce2ad
kkuratolodon <mif05123@gmail.com>
2024-04-26 00:27:01 +0700

    [REFACTOR] Add more test for CouponRepository

--- src/test/java/com/heymart/coupon/repository/CouponRepositoryTest.java
+++ src/test/java/com/heymart/coupon/repository/CouponRepositoryTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.dao.DataIntegrityViolationException;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,18 +25,125 @@ public class CouponRepositoryTest {
 
     @Test
     public void testSaveCoupon() {
-        Coupon coupon = new Coupon();
-        coupon.setId("123");
-        coupon.setType("Product");
-        coupon.setPercentDiscount(10);
-        coupon.setFixedDiscount(5);
-        coupon.setMaxDiscount(50);
-        coupon.setSupermarket("LocalMart");
-        coupon.setIdProduct("P001");
-        coupon.setMinTransaction(0);
+        Coupon coupon = new CouponBuilder()
+                .setType(CouponType.PRODUCT.getValue())
+                .setPercentDiscount(20)
+                .setFixedDiscount(100)
+                .setMaxDiscount(300)
+                .setSupermarket("Indomaret")
+                .setIdProduct("P01")
+                .getResult();
 
         Coupon savedCoupon = couponRepository.save(coupon);
         assertNotNull(savedCoupon);
-        assertEquals("Product", savedCoupon.getType());
+        assertEquals("PRODUCT", savedCoupon.getType());
+    }
+    @Test
+    public void testFindAllCoupon() {
+        Coupon coupon = new CouponBuilder()
+                .setType(CouponType.PRODUCT.getValue())
+                .setPercentDiscount(20)
+                .setFixedDiscount(100)
+                .setMaxDiscount(300)
+                .setSupermarket("Indomaret")
+                .setIdProduct("P01")
+                .getResult();
+
+        List<Coupon> couponList = couponRepository.findAll();
+        assertEquals(couponList.size(),0);
+        Coupon savedCoupon = couponRepository.save(coupon);
+        couponList = couponRepository.findAll();
+        assertEquals(couponList.size(),1);
+    }
+    @Test
+    public void testFindByIdCoupon() {
+        Coupon coupon = new CouponBuilder()
+                .setType(CouponType.PRODUCT.getValue())
+                .setPercentDiscount(20)
+                .setFixedDiscount(100)
+                .setMaxDiscount(300)
+                .setSupermarket("Indomaret")
+                .setIdProduct("P01")
+                .getResult();
+
+
+        couponRepository.save(coupon);
+        Coupon couponFound = couponRepository.findById(coupon.getId()).get();
+        assertEquals(coupon.getId(),couponFound.getId());
+    }
+    @Test
+    public void testFindByIdCouponNotExist() {
+        Coupon coupon = new CouponBuilder()
+                .setType(CouponType.PRODUCT.getValue())
+                .setPercentDiscount(20)
+                .setFixedDiscount(100)
+                .setMaxDiscount(300)
+                .setSupermarket("Indomaret")
+                .setIdProduct("P01")
+                .getResult();
+
+        couponRepository.save(coupon);
+        assertThrows(NoSuchElementException.class, () -> {
+            Coupon couponFound = couponRepository.findById("NOT_VALID_ID").get();
+        });
+    }
+    @Test
+    public void testDeleteCouponSuccess() {
+        Coupon coupon = new CouponBuilder()
+                .setType(CouponType.PRODUCT.getValue())
+                .setPercentDiscount(20)
+                .setFixedDiscount(100)
+                .setMaxDiscount(300)
+                .setSupermarket("Indomaret")
+                .setIdProduct("P01")
+                .getResult();
+
+        Coupon savedCoupon = couponRepository.save(coupon);
+        assertEquals(couponRepository.findAll().size(),1);
+        couponRepository.delete(coupon);
+        assertEquals(couponRepository.findAll().size(),0);
+    }
+    @Test
+    public void testDeleteCouponNotExist() {
+        Coupon coupon = new CouponBuilder()
+                .setType(CouponType.PRODUCT.getValue())
+                .setPercentDiscount(20)
+                .setFixedDiscount(100)
+                .setMaxDiscount(300)
+                .setSupermarket("Indomaret")
+                .setIdProduct("P01")
+                .getResult();
+
+        Coupon coupon2 = new CouponBuilder()
+                .setType(CouponType.PRODUCT.getValue())
+                .setPercentDiscount(20)
+                .setFixedDiscount(100)
+                .setMaxDiscount(300)
+                .setSupermarket("Indomaret")
+                .setIdProduct("P01")
+                .getResult();
+
+        couponRepository.save(coupon);
+        assertEquals(couponRepository.findAll().size(),1);
+        couponRepository.delete(coupon2);
+        assertEquals(couponRepository.findAll().size(),1);
+    }
+    @Test
+    public void testUpdateCoupon() {
+        Coupon coupon = new CouponBuilder()
+                .setType(CouponType.PRODUCT.getValue())
+                .setPercentDiscount(20)
+                .setFixedDiscount(100)
+                .setMaxDiscount(300)
+                .setSupermarket("Indomaret")
+                .setIdProduct("P01")
+                .getResult();
+
+        Coupon savedCoupon = couponRepository.save(coupon);
+        assertEquals(savedCoupon.getMaxDiscount(), 300);
+        coupon.setMaxDiscount(250);
+        couponRepository.save(coupon);
+        Coupon updatedCoupon = couponRepository.findById(coupon.getId()).orElseThrow();
+        assertEquals(updatedCoupon.getMaxDiscount(), 250);
     }
 }
