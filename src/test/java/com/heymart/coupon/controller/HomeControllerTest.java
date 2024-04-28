326210ea95adc53fe3e4b2e2bbba6e388c6d0ad2
kkuratolodon <mif05123@gmail.com>
2024-04-28 21:04:29 +0700

    [RED] Add test for create coupon in controller

--- src/test/java/com/heymart/coupon/controller/HomeControllerTest.java
+++ src/test/java/com/heymart/coupon/controller/HomeControllerTest.java
@@ -1,30 +1,49 @@
 package com.heymart.coupon.controller;
 
+import com.heymart.coupon.model.Coupon;
+import com.heymart.coupon.model.builder.CouponBuilder;
+import com.heymart.coupon.model.enums.CouponType;
+import com.heymart.coupon.service.CouponService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 @ExtendWith(MockitoExtension.class)
 class HomeControllerTest {
     MockMvc mockMvc;
+    @Mock
+    private CouponService couponService;
     @InjectMocks
     HomeController homeController;
 
     @BeforeEach
     void setup() {
+        MockitoAnnotations.initMocks(this);
         mockMvc = MockMvcBuilders.standaloneSetup(homeController).build();
     }
 
     @Test
-    void helloWorld() throws Exception {
+    void testHelloWorld() throws Exception {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Hello World!"));
@@ -38,14 +57,27 @@ class HomeControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string("Ini isinya list kupon dari " + supermarket + "!"));
     }
-
     @Test
     void testCreateCoupon() throws Exception {
         String supermarket = "Raflimart";
+        Coupon expectedCoupon = new CouponBuilder()
+                .setType(CouponType.PRODUCT.getValue())
+                .setPercentDiscount(20)
+                .setFixedDiscount(100)
+                .setMaxDiscount(300)
+                .setSupermarket(supermarket)
+                .setIdProduct("P01")
+                .getResult();
 
-        mockMvc.perform(post("/create/" + supermarket))
+        when(couponService.createCoupon(any(Coupon.class))).thenReturn(expectedCoupon);
+
+        mockMvc.perform(post("/create")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("supermarket", supermarket))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Pembuatan kupon untuk supermarket " + supermarket + "!"));
+
+        verify(couponService, times(1)).createCoupon(argThat(coupon -> coupon.getSupermarket().equals(supermarket)));
     }
     @Test
     void testUpdateCoupon() throws Exception {
