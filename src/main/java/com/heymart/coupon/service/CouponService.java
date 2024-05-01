b737637a2c7718bdce51ab2af220ea088c24b826
kkuratolodon <mif05123@gmail.com>
2024-05-01 17:58:37 +0700

    [GREEN] implement coupon service

--- src/main/java/com/heymart/coupon/service/CouponService.java
+++ src/main/java/com/heymart/coupon/service/CouponService.java
@@ -1,12 +1,17 @@
 package com.heymart.coupon.service;
 
 import com.heymart.coupon.model.Coupon;
+import com.heymart.coupon.model.ProductCoupon;
+import com.heymart.coupon.model.TransactionCoupon;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public interface CouponService {
-    public Coupon createCoupon(Coupon coupon);
-    public Coupon edit(Coupon editedCoupon);
-    public Coupon delete(Coupon editedCoupon);
-    public List<Coupon> findAll();
+    public ProductCoupon createOrUpdateProductCoupon(ProductCoupon coupon);
+    public TransactionCoupon createOrUpdateTransactionCoupon(TransactionCoupon editedCoupon);
+    public void deleteProductCoupon(ProductCoupon coupon);
+    public void deleteTransactionCoupon(TransactionCoupon coupon);
+    public List<ProductCoupon> findAllProductCoupons();
+    public List<TransactionCoupon> findAllTransactionCoupons();
 }
--- src/main/java/com/heymart/coupon/service/CouponServiceImpl.java
+++ src/main/java/com/heymart/coupon/service/CouponServiceImpl.java
@@ -1,7 +1,9 @@
 package com.heymart.coupon.service;
 
-import com.heymart.coupon.model.Coupon;
-import com.heymart.coupon.repository.CouponRepository;
+import com.heymart.coupon.model.ProductCoupon;
+import com.heymart.coupon.model.TransactionCoupon;
+import com.heymart.coupon.repository.ProductCouponRepository;
+import com.heymart.coupon.repository.TransactionCouponRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,21 +11,36 @@ import java.util.List;
 
 @Service
 public class CouponServiceImpl implements CouponService{
+
     @Autowired
-    private CouponRepository couponRepository;
-    public Coupon createCoupon(Coupon coupon) {
-        couponRepository.save(coupon);
-        return coupon;
+    private ProductCouponRepository productCouponRepository;
+
+    @Autowired
+    private TransactionCouponRepository transactionCouponRepository;
+
+    public ProductCoupon createOrUpdateProductCoupon(ProductCoupon coupon) {
+        return productCouponRepository.save(coupon);
     }
-    public Coupon edit(Coupon editedCoupon) {
-        couponRepository.save(editedCoupon);
-        return editedCoupon;
+
+    public TransactionCoupon createOrUpdateTransactionCoupon(TransactionCoupon coupon) {
+        return transactionCouponRepository.save(coupon);
     }
-    public Coupon delete(Coupon editedCoupon) {
-        couponRepository.delete(editedCoupon);
-        return editedCoupon;
+
+    public void deleteTransactionCoupon(TransactionCoupon coupon) {
+        transactionCouponRepository.delete(coupon);
     }
-    public List<Coupon> findAll() {
-        return couponRepository.findAll();
+
+    public void deleteProductCoupon(ProductCoupon coupon) {
+        productCouponRepository.delete(coupon);
     }
+
+    public List<ProductCoupon> findAllProductCoupons() {
+        return productCouponRepository.findAll();
+    }
+
+    public List<TransactionCoupon> findAllTransactionCoupons() {
+        return transactionCouponRepository.findAll();
+    }
+
+    // Additional methods to handle business logic
 }
