8ed2376a4f32a3b149474b0d9f11b0c257728378
kkuratolodon <mif05123@gmail.com>
2024-05-04 21:04:09 +0700

    [BUILD] Add sonarcloud workflows

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.springframework.boot") version "3.2.4"
     id("io.spring.dependency-management") version "1.1.4"
     jacoco
+    id("org.sonarqube") version "4.4.1.3373"
 }
 
 group = "com.heymart"
@@ -59,5 +60,12 @@ tasks.jacocoTestReport {
         xml.required.set(false)
         csv.required.set(false)
         html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+    }
+}
+sonar {
+    properties {
+        property("sonar.projectKey", "AdvPro-B13_heymart-be-coupon")
+        property("sonar.organization", "advpro-b13")
+        property("sonar.host.url", "https://sonarcloud.io")
     }
 }
