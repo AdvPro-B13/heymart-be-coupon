b5ae96aa112168256cefe04445786aff0f96c872
kkuratolodon <mif05123@gmail.com>
2024-05-23 11:04:42 +0700

    [MONITORING] Update monitoring configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("io.micrometer:micrometer-registry-prometheus")
 
 
     compileOnly("org.projectlombok:lombok")
